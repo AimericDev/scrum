7f2747742eade2df3a0eb986132fc3bfadf634e1
Nicolas-IMIE <nicolas.lepiez@imie-rennes.fr>
2017-06-21 16:38:15 +0200

    Affichage

--- src/fr/imie/scrum_calculator/main/Calculator.java
+++ src/fr/imie/scrum_calculator/main/Calculator.java
@@ -1,9 +1,70 @@
 package fr.imie.scrum_calculator.main;
 
+import java.util.Scanner;
+
 public class Calculator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		while(1 > 0)
+		{
+			int number_one = 0;
+			int number_two = 0;
+			int result = 0;
+			boolean opOk = false;
+			String operator = "";
+			
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.println("Sélectionner deux nombres entiers pour réaliser une opération : ");
+			
+			System.out.println("Premier nombre : ");
+			number_one = sc.nextInt();
+			
+			System.out.println("Deuxième nombre : ");
+			number_two = sc.nextInt();
+
+			
+			while(opOk == false)
+			{
+				System.out.println("Entrez l'opérateur que vous souhaitez utiliser (+ - * /) : ");
+				operator = sc.nextLine();
+				if(operator.equals("+") || operator.equals("-") || operator.equals("*") || operator.equals("/"))
+					opOk = true;
+				else
+					System.out.println("Opérateur invalide !");
+			}
+
+			
+			Operation operation = new Operation();
+			
+			switch(operator)
+			{
+				case "+":
+					System.out.println("Votre opération : " + number_one + " + " + number_two);
+					result = operation.addition(number_one, number_two);
+					break;
+				case "-":
+					System.out.println("Votre opération : " + number_one + " - " + number_two);
+					result = operation.soustraction(number_one, number_two);
+					break;
+				case "*":
+					System.out.println("Votre opération : " + number_one + " * " + number_two);
+					result = operation.multiplication(number_one, number_two);
+					break;
+				case "/":
+					System.out.println("Votre opération : " + number_one + " / " + number_two);
+					result = operation.division(number_one, number_two);
+					break;
+				default:
+					System.out.println("Votre opérateur est invalide, par défaut votre opération : " + number_one + " + " + number_two);
+					result = operation.addition(number_one, number_two);
+			}
+			
+			System.out.println("Le résultat de votre opération est : " + result + "\n");
+		}
+		
+		
 
 	}
 
