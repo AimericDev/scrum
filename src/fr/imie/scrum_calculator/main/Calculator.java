97246e9f5a69c88715ac34404630a834a1ed2780
Jérôme Pavic <jeromepavic@gmail.com>
2017-06-21 16:29:36 +0200

    798 Stocker les données saisies et le résultat dans un fichier in progress

--- src/fr/imie/scrum_calculator/main/Calculator.java
+++ src/fr/imie/scrum_calculator/main/Calculator.java
@@ -1,94 +1,110 @@
 package fr.imie.scrum_calculator.main;
 
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Calculator {
 
 	public static void main(String[] args) {
 		
-		while(1 > 0)
-		{
-			int number_one = 0;
-			int number_two = 0;
-			float result = 0;
-			boolean opOk = false;
-			boolean nbOk = false;
-			String operator = "";
-			
-			Scanner sc = new Scanner(System.in);
-			
+		try{
+		    PrintWriter writer = new PrintWriter("./calculator.log", "UTF-8");
+		    
 
-			do 
+		    
+		
+			while(1 > 0)
 			{
-				System.out.println("Entrez l'opérateur que vous souhaitez utiliser (+ - * /) -- Opérateur par défaut : + : ");
-				operator = sc.nextLine();
 				
-				if(operator.equals("+") || operator.equals("-") || operator.equals("*") || operator.equals("/"))
+				int number_one = 0;
+				int number_two = 0;
+				float result = 0;
+				boolean opOk = false;
+				boolean nbOk = false;
+				String operator = "";
+				
+				Scanner sc = new Scanner(System.in);
+				
+	
+				do 
 				{
-					opOk = true;
-				}
-				else
-				{
-					System.out.println("Opérateur invalide !");
-				}
-			}
-			while(opOk == false);
-			
-			
-			while (nbOk == false) {
-				System.out.println("Sélectionner deux nombres entiers pour réaliser une opération : ");
-				
-				System.out.println("Premier nombre : ");
-				
-				if (sc.hasNextInt()){
-					number_one = sc.nextInt();
+					System.out.println("Entrez l'opérateur que vous souhaitez utiliser (+ - * /) -- Opérateur par défaut : + : ");
+					operator = sc.nextLine();
 					
+					if(operator.equals("+") || operator.equals("-") || operator.equals("*") || operator.equals("/"))
+					{
+						opOk = true;
+					}
+					else
+					{
+						System.out.println("Opérateur invalide !");
+					}
+				}
+				while(opOk == false);
+				
+				
+				while (nbOk == false) {
+					System.out.println("Sélectionner deux nombres entiers pour réaliser une opération : ");
 					
-					System.out.println("Deuxième nombre : ");
-					if (sc.hasNextInt()){	
-						number_two = sc.nextInt();
-						nbOk =true;
+					System.out.println("Premier nombre : ");
+					
+					if (sc.hasNextInt()){
+						number_one = sc.nextInt();
+						
+						
+						System.out.println("Deuxième nombre : ");
+						if (sc.hasNextInt()){	
+							number_two = sc.nextInt();
+							nbOk =true;
+						}
+						sc.nextLine();
 					}
 					sc.nextLine();
 				}
-				sc.nextLine();
+				
+				
+				
+				
+				
+	
+				
+	
+				
+				Operation operation = new Operation();
+				
+				switch(operator)
+				{
+					case "+":
+						System.out.println("Votre opération : " + number_one + " + " + number_two);
+						result = operation.addition(number_one, number_two);
+						break;
+					case "-":
+						System.out.println("Votre opération : " + number_one + " - " + number_two);
+						result = operation.soustraction(number_one, number_two);
+						break;
+					case "*":
+						System.out.println("Votre opération : " + number_one + " * " + number_two);
+						result = operation.multiplication(number_one, number_two);
+						break;
+					case "/":
+						System.out.println("Votre opération : " + number_one + " / " + number_two);
+						result = operation.division(number_one, number_two);
+						break;
+					default:
+						System.out.println("Votre opérateur est invalide, par défaut votre opération : " + number_one + " + " + number_two);
+						result = operation.addition(number_one, number_two);
+				}
+				
+				System.out.println("Le résultat de votre opération est : " + result + "\n");
+				writer.println(result);
+
 			}
 			
 			
 			
-			
-			
-
-			
-
-			
-			Operation operation = new Operation();
-			
-			switch(operator)
-			{
-				case "+":
-					System.out.println("Votre opération : " + number_one + " + " + number_two);
-					result = operation.addition(number_one, number_two);
-					break;
-				case "-":
-					System.out.println("Votre opération : " + number_one + " - " + number_two);
-					result = operation.soustraction(number_one, number_two);
-					break;
-				case "*":
-					System.out.println("Votre opération : " + number_one + " * " + number_two);
-					result = operation.multiplication(number_one, number_two);
-					break;
-				case "/":
-					System.out.println("Votre opération : " + number_one + " / " + number_two);
-					result = operation.division(number_one, number_two);
-					break;
-				default:
-					System.out.println("Votre opérateur est invalide, par défaut votre opération : " + number_one + " + " + number_two);
-					result = operation.addition(number_one, number_two);
+		} catch (Exception e) {
+			System.out.println("Probl�me lors de la cr�ation du fichier d'historique");
 			}
-			
-			System.out.println("Le résultat de votre opération est : " + result + "\n");
-		}
 		
 		
 
